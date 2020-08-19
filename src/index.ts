a80f779c8927a4a44f5d2ef25e656f9a7f46b8e6
Christopher Edwards <cedwards036@gmail.com>
2020-08-19 19:38:34 -0400

    update port to work with Heroku

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at https://localhost:${PORT}`);

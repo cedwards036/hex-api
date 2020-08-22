7cf11750567ee59fb39cb48b148c220d7b8f80d0
Christopher Edwards <cedwards036@gmail.com>
2020-08-22 09:44:36 -0400

    add proof-of-concept db connection

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,25 @@
 import express from 'express';
+import { Pool } from 'pg';
 
 const app = express();
 
-app.get('/', (req, res) => res.status(200).send('Hex server'));
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
+
+app.set('view engine', 'html');
+app.get('/', (req, res) => res.status(200).send('Hex server'))
+    .get('/db', async (req, res) => {
+        try {
+            const client = await pool.connect();
+            const result = await client.query('SELECT * FROM test_table');
+            const results = { 'results': (result) ? result.rows : null};
+            res.json({name: results});
+            client.release();
+        } catch (err) {
+            console.error(err);
+            res.send("Error " + err);
+        }
+    })
 
 export default app;
